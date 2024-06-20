e91dba0da6adbeab8e9cc175cd2cf4cec0e0b536
RyJaxxon <ryanjaxxon@gmail.com>
2024-06-20 17:38:25 -0400

    completed assignment

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,15 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, options = {}) => {
+    try {
+        const response = await fetch(url, options)
+        if (!response.ok) throw new Error(`Fetch failed with status - [${response.status}], [${response.statusText}]`)
+        const isJson = (response.headers.get('content-type') || '').includes('application/json');
+        let data = ''
+        isJson ? data = await response.json() : data = await response.text();
+        return [data, null];
+    } catch (error) {
+        console.warn(error);
+        return [null, error];
+    }
 };
+
+fetchHandler();
